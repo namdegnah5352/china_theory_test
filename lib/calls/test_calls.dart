318b8ba62d06cd8ab2e4a13cc6740f574ce8236a
namdegnah5352 <132438431+namdegnah5352@users.noreply.github.com>
2023-08-18 08:24:01 +0100

    Update test_calls.dart

--- lib/calls/test_calls.dart
+++ lib/calls/test_calls.dart
@@ -1,6 +1,11 @@
 import 'app_calls.dart' as appcalls;
 import '../config/constants.dart';
 
+int _mark;
+
+set mark(int mark) => _mark = mark;
+int get mark => _mark;
+
 void finishTest(bool method, int? mark) async {
   if (method == AppConstants.questionsFinished) {
     //appcalls.resetCounter();
